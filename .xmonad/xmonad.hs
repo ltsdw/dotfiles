49a96d471b91ad7701e823b689ba0b02b08de0d2
ltsdw <alandms123@gmail.com>
2020-01-02 00:55:15 -0300

    ...

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -212,7 +212,7 @@ myMouseBindings (XConfig {modMask = modMask}) = M.fromList $
 
 -- StartupHook
 myStartupHook = do
-        spawn "picom --config ~/.config/picom/compton.conf"
+        spawnOnce "picom --config ~/.config/picom/compton.conf"
         spawnOnce "feh --no-fehbg --bg-fill ~/.backgrounds/Riven-Picture.png"
         spawnOnce "xsetroot -cursor_name left_ptr"
 
